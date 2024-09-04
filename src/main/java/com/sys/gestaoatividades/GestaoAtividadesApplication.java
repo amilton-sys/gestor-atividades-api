f797d709b644f0156ac9c8a31a3e1b3a0244267b
Amilton <amiltonjose.pe@gmail.com>
2024-09-04 16:50:53 -0300

    projeto criado

---
+++ src/main/java/com/sys/gestaoatividades/GestaoAtividadesApplication.java
@@ -0,0 +1,13 @@
+package com.sys.gestaoatividades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoAtividadesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoAtividadesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sys/gestaoatividades/GestaoAtividadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sys.gestaoatividades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoAtividadesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
